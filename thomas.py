fb6033d30fdfd5b47ba0a8b87d481bb610c6f1b0
mirkan1 <32144322+mirkan1@users.noreply.github.com>
2018-12-01 15:38:19 +0300

    Update thomas.py

--- thomas.py
+++ thomas.py
@@ -3,6 +3,11 @@ from os import listdir
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+# Discord scroll table that includes people in it
+scroll_div_class = "scroller-2FKFPG firefoxFixScrollFlex-cnI2ix members-1998pB"
+user_div_class = "nameTag-3p0yK- nameTag-m8r81H"
+span_class = "usernameOnline-3jr_0Y username-1cB_5E"
+
 # PROFILE_DIR = 'C://Users//Raq//AppData//Local//Mozilla//Firefox'
 # profile = webdriver.FirefoxProfile(PROFILE_DIR + '//%s//' % listdir(PROFILE_DIR)[0])
 
